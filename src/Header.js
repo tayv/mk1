a4214593058a72daabd9e9da7e0f1a47af377553
tayv <48400779+tayv@users.noreply.github.com>
2021-08-06 14:24:58 -0600

    Setup header and table components

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="App-header">
+            <div className="App-logo">
+            LOGO
+            </div>
+            <div className="Navigation">
+            Leaderboard    Schedule    Race Results
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/Leaderboard.js
@@ -0,0 +1,95 @@
+import React from 'react'; 
+import './Leaderboard.css';
+  
+const Leaderboard = () => {
+  return  (
+    <div className="Leaderboard-container">
+        <table>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Racer</th>
+                    <th>Points</th>
+                    <th>Change</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>🏆</td>
+                    <td>Racer 1</td>
+                    <td>0</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>Racer 2</td>
+                    <td>0</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>Racer 2</td>
+                    <td>0</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>Racer 2</td>
+                    <td>0</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>Racer 2</td>
+                    <td>0</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>Racer 2</td>
+                    <td>0</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>Racer 2</td>
+                    <td>0</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>Racer 2</td>
+                    <td>0</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>Racer 2</td>
+                    <td>0</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>Racer 2</td>
+                    <td>0</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>Racer 2</td>
+                    <td>0</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>Racer 2</td>
+                    <td>0</td>
+                    <td>-</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  )
+}
+  
+export default Leaderboard;
