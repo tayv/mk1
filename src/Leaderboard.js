b40ea93f2d0507eddd94698489ecc530124d4748
tayv <48400779+tayv@users.noreply.github.com>
2021-08-07 15:15:44 -0600

    Made SeasonResults table data its own component

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -1,17 +1,18 @@
 import React, { useState } from 'react'; 
-import racerData from './racer-data.json';
 import './Leaderboard.css';
+import SeasonResults from './SeasonResults.js';
   
 const Leaderboard = () => {
-    const [racers, setRacers] = useState(racerData);
+   
 
     return  (
         <div className="Leaderboard-container">
             <div className="Leaderboard-filter"> 
                 <label for="season" style={{visibility: "hidden"}}>Season: </label>
                 <select name="season" id="season">
-                    <option value="s1">Season 1</option>
-                    <option value="s2">Season 2</option>
+                    <option value="season1">Season 1</option>
+                    <option value="season2">Season 2</option>
+                    <option value="alltime">All Time</option>
                 </select> 
             </div>
         
@@ -25,17 +26,7 @@ const Leaderboard = () => {
                         <th>Change</th>
                     </tr>
                 </thead>
-                <tbody>
-                    { racers.map( (racer) => (
-                        <tr>
-                            <td>{ racer.trophy }</td>
-                            <td>{ racer.avatar }</td>
-                            <td>{ racer.name }</td>
-                            <td>{ racer.points }</td>
-                            <td>{ racer.change }</td>
-                        </tr>
-                    )) }
-                </tbody>
+                    <SeasonResults />
             </table>
         </div>
 
