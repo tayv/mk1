24bfda41560a85de6c16d30b838ed6af515bdb73
tayv <48400779+tayv@users.noreply.github.com>
2021-08-11 16:25:34 -0600

    googlespreadsheets basic functionality working
    able to get row data

--- src/components/SeasonResults/SeasonResults.js
+++ src/components/SeasonResults/SeasonResults.js
@@ -1,12 +1,33 @@
 import React, { useState, useEffect } from 'react'; 
 import axios from 'axios';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
 
 const spreadsheetID = process.env.REACT_APP_GOOGLE_SPREADSHEET_ID;
 const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
 
+const doc = new GoogleSpreadsheet(spreadsheetID);
+doc.useApiKey(apiKey);
+
+
 const SeasonResults = (props) => {
     const [racers, setRacers] = useState([]);
 
+    useEffect(() => {
+
+        (async function() {
+            await doc.loadInfo();
+            console.log(doc.title)
+            const sheet = doc.sheetsByIndex[0];
+            console.log(sheet);
+            const rows = await sheet.getRows(); // can pass in { limit, offset }
+
+            // read/write row values
+            console.log(rows[0].championships);
+            console.log(sheet.headerValues[2])
+          }());
+     
+    }, [props.season]); 
+    /*
     useEffect(() => {
         const fetchData = async () => {
             const fetchURL = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetID}?key=${apiKey}` ; 
@@ -19,7 +40,7 @@ const SeasonResults = (props) => {
         fetchData();
 
     }, [props.season]);
-
+*/
 
     return (
         <tbody>
