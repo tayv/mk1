237a6e27f9c447c807fe883edeca4efd55fa26a3
tayv <48400779+tayv@users.noreply.github.com>
2021-08-13 14:09:05 -0600

    Update racerList function added

--- src/components/SeasonResults/SeasonResults.js
+++ src/components/SeasonResults/SeasonResults.js
@@ -5,9 +5,11 @@ import { GoogleSpreadsheet } from 'google-spreadsheet';
 const spreadsheetID = process.env.REACT_APP_GOOGLE_SPREADSHEET_ID;
 const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
 
+// For google spreadsheet api refer to https://theoephraim.github.io/node-google-spreadsheet/#/?id=working-with-rows
 const doc = new GoogleSpreadsheet(spreadsheetID);
 doc.useApiKey(apiKey);
 
+const sheetDataRacerList = [];
 
 const SeasonResults = (props) => {
     const [racers, setRacers] = useState([]);
@@ -16,14 +18,44 @@ const SeasonResults = (props) => {
 
         (async function() {
             await doc.loadInfo();
-            console.log(doc.title)
-            const sheet = doc.sheetsByIndex[0];
-            console.log(sheet);
-            const rows = await sheet.getRows(); // can pass in { limit, offset }
-
+          //  console.log(doc)
+            const sheetRacerList = doc.sheetsByIndex[0];
+           // console.log("racer list ", sheetRacerList.rowCount)
+            const sheetSeason1 = doc.sheetsByIndex[1];
+            const sheetSeason2 = doc.sheetsByIndex[2];
+            // console.log(sheet);
+            const rowsRacerList = await sheetRacerList.getRows(); // can pass in { limit, offset }
+            const rowsSeason1 = await sheetSeason1.getRows();
+            const rowsSeason2 = await sheetSeason2.getRows();
+            console.log("rowsRacerList: ", rowsRacerList)
             // read/write row values
-            console.log(rows[0].championships);
-            console.log(sheet.headerValues[2])
+
+            const updateRacer = (rowLimit) => {
+                // updateRacer() updates racers state object 
+                // rowLimit is a number used to limit the number of rows looped through. Should be equal to number of racers +1 (because of heading row)
+                for (let i=0; i<rowLimit; i++) {
+                   sheetDataRacerList[i]= {
+                       id: rowsRacerList[i].id,
+                       avatar: rowsRacerList[i].avatar,
+                       name: rowsRacerList[i].name,
+                       allTime: {
+                           championships: rowsRacerList[i].championships,
+                           participated: rowsRacerList[i].participated
+                       }
+
+                    };
+                  console.log(sheetDataRacerList)
+                 }
+            }
+            
+             
+           
+            
+            
+            updateRacer(13);
+          
+            console.log("sheeDataRacerList: ", sheetDataRacerList);
+           // console.log(sheet.headerValues[2])
           }());
      
     }, [props.season]); 
@@ -49,7 +81,6 @@ const SeasonResults = (props) => {
                     <td>{ racer.data.title }</td>
                 </tr>
             )) }
-          {console.log(racers)}
         </tbody>
     )
 }
