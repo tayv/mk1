f53de66a4ca06623333b8cfaa9efe1166612df32
tayv <48400779+tayv@users.noreply.github.com>
2021-08-31 15:03:23 -0600

    Removed axios
    No longer needed as using google api

--- src/components/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseurl: "https://jsonplaceholder.typicode.com/todos/1"
-})
-
-export default instance;
-
-// currently unused. may use env variable instead
