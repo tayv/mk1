5e0514638022c3795e11d275b901e37784c91a96
tayv <48400779+tayv@users.noreply.github.com>
2021-09-16 13:53:48 -0600

    Added checkboard pattern

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,9 @@ import Leaderboard from '../Leaderboard/Leaderboard.js';
 function App() {
   return (
     <div className="flex flex-col w-screen content-start justify-center items-center text-xs sm:text-lg md:text-xl text-gray-600">
+      <div className="bg-white">
+        <img className="w-screen h-5" src="./bkg_checkerboard.svg" alt="pattern" />
+      </div>
       <Header />
       <Leaderboard />
     </div>
