e7d7e3d72201edc47139def98a9ae0fa14083a66
tayv <48400779+tayv@users.noreply.github.com>
2023-10-03 12:55:10 -0600

    REFACTOR: Cleaned up useGoogleSheetData hook

--- src/hooks/useGoogleSheetData.js
+++ src/hooks/useGoogleSheetData.js
@@ -5,164 +5,107 @@ import { merge } from "lodash"
 const spreadsheetID = process.env.REACT_APP_GOOGLE_SPREADSHEET_ID
 const apiKey = process.env.REACT_APP_GOOGLE_API_KEY
 
-// This needs to be refactored for cleaner async fetching. Maybe bundle everything using Promise.all()
-
-// For google spreadsheet api refer to https://theoephraim.github.io/node-google-spreadsheet/#/?id=working-with-rows
 const doc = new GoogleSpreadsheet(spreadsheetID)
 doc.useApiKey(apiKey)
 
+const fetchRacerListRows = async (sheet) => {
+  const rowsAllTime = await sheet.getRows({ limit: 24 }) // returns 25 racers because array starts at 0
+  return rowsAllTime.filter((row) => row.id && row.id.trim() !== "") // filter empty rows
+}
+
+const fetchSeasonRows = async (sheet) => {
+  const rowsSeasonList = await sheet.getRows({ limit: 15 }) // returns 16 racers
+  let seasonResults = []
+  let seasonResultsTeam = []
+
+  await Promise.all(
+    rowsSeasonList.map(async (row) => {
+      seasonResults.push({
+        rank: row.rank,
+        name: row.name,
+        avatar: row.avatar,
+        points: row.points,
+        change: row.change,
+      })
+
+      if (row.teamName !== undefined) {
+        seasonResultsTeam.push({
+          teamRank: row.teamRank,
+          teamName: row.teamName,
+          teamPoints: row.teamPoints,
+          teamChange: row.teamChange,
+        })
+      }
+    })
+  )
+
+  return {
+    seasonResults: merge(seasonResults),
+    seasonResultsTeam: merge(seasonResultsTeam),
+  }
+}
+
 export const useGoogleSheetData = () => {
   const [sheetsAll, setSheetData] = useState([])
-  const [statsAllTime, setStatsAllTime] = useState([]) // allTime stats by racer used to display alltime results
+  const [statsAllTime, setStatsAllTime] = useState([])
   const [rowsAllTime, setRowsAllTime] = useState([])
   const [statsBySeason, setStatsBySeason] = useState({})
   const [statsByTeamSeason, setTeamStatsBySeason] = useState({})
-  const [isDataLoaded, setIsDataLoaded] = useState(false) // used to prevent rendering empty leaderboard
-  const [dataFetched, setDataFetched] = useState(false) // Added to patch the initial load bug
+  const [isDataLoaded, setIsDataLoaded] = useState(false)
   const [error, setError] = useState(null)
 
   useEffect(() => {
-    const fetchSheetData = async () => {
+    const fetchData = async () => {
       try {
-        // load the google sheet
         await doc.loadInfo()
-
-        // get an array of all sheets as objects. Need to use sheetsByIndex so that it's an array.
         const sheetsAll = doc.sheetsByIndex
-        // save to state
         setSheetData(sheetsAll)
-        // set data loaded to true so we can render the leaderboard data
-        setDataFetched(true)
-      } catch (error) {
-        console.error("Error fetching data:", error)
-        setError(error.message) // Here we're capturing the error message
-      }
-    }
 
-    fetchSheetData()
-  }, []) //  only need to run once on page render
-
-  useEffect(() => {
-    const fetchRowData = async () => {
-      if (!dataFetched) return
-
-      sheetsAll.forEach((sheet) => {
-        // Regex used to check if sheet name contains string "season" so we can get rows from the right sheet
-        const regexMatchSeason = /season/gm
-
-        if (sheet.title === "allTime") {
-          // If we're on the allTime sheet then grab all the row data and save it in state so we can work with it
-          async function fetchRacerListRows() {
-            // Get all the active rows for this sheet
-            // Will break if more than 25 all time racers. Using a limit so we can use Google Sheet sorting formulas
-            const rowsAllTime = await sheet.getRows({ limit: 24 })
-
-            // Filter out rows with blank or undefined id values
-            const filteredRowsAllTime = rowsAllTime.filter(
-              (row) => row.id && row.id.trim() !== ""
+        for (const sheet of sheetsAll) {
+          if (sheet.title === "allTime") {
+            const allTimeRows = await fetchRacerListRows(sheet)
+            setRowsAllTime(allTimeRows)
+          } else if (/season/gm.test(sheet.title)) {
+            const { seasonResults, seasonResultsTeam } = await fetchSeasonRows(
+              sheet
             )
-
-            // save the data to state so we can trigger other useEffects
-            setRowsAllTime(filteredRowsAllTime)
-            // set data loaded to true so we can render the leaderboard data
+            setStatsBySeason((prev) => ({
+              ...prev,
+              [sheet.title]: seasonResults,
+            }))
+            setTeamStatsBySeason((prev) => ({
+              ...prev,
+              [sheet.title]: seasonResultsTeam,
+            }))
           }
-          if (dataFetched) {
-            fetchRacerListRows()
-          }
-        } else if (sheet.title.match(regexMatchSeason)) {
-          // If we're on a sheet starting with "season" then get each row and deep copy it into the statsBySeason object
-          async function fetchSeasonRows() {
-            // TODO: limit should be updated to be dynamic based on number of racers in allTime sheet.
-            // This will break if more than 14 racers added in a season (note array count starts at 0 so limit = total racers + 1)
-            const rowsSeasonList = await sheet.getRows({ limit: 15 })
-
-            // Initialize array to hold each season's individual and team row results
-            let seasonResults = []
-            let seasonResultsTeam = []
-            let seasonTeamResult
-
-            // Promise.all groups promises together in an iterable array and prevents race conditions
-            await Promise.all(
-              rowsSeasonList.map(async (row) => {
-                // Each row represents a racers results for that season
-                // Save desired stats to an object
-                let seasonIndividualResult = {
-                  rank: row.rank,
-                  name: row.name,
-                  avatar: row.avatar,
-                  points: row.points,
-                  change: row.change,
-                }
-
-                // Wrap in condition to prevent saving undefined team rows due to google sheets layout since there's less teams than individual racers
-                if (row.teamName !== undefined) {
-                  seasonTeamResult = {
-                    teamRank: row.teamRank,
-                    teamName: row.teamName,
-                    teamPoints: row.teamPoints,
-                    teamChange: row.teamChange,
-                  }
-
-                  // do this inside conditional so that undefined values aren't added
-                  seasonResultsTeam = [...seasonResultsTeam, seasonTeamResult]
-                }
-
-                // add each racer's results to the respective array
-                seasonResults = [...seasonResults, seasonIndividualResult]
-              })
-            )
-
-            // Use Lodash's merge() to deep copy the array of all the racer results objects into the statsBySeason object under their respective season
-            // Assign the row results to the correct season
-            statsBySeason[sheet.title] = merge(seasonResults)
-            statsByTeamSeason[sheet.title] = merge(seasonResultsTeam)
-
-            // Update state
-            setStatsBySeason(statsBySeason)
-            setTeamStatsBySeason(statsByTeamSeason)
-            // set data loaded to true so we can render the leaderboard data
-            setIsDataLoaded(true)
-          }
-
-          fetchSeasonRows()
-        } else {
-          // Something went wrong. Likely a new sheet was added that didn't follow naming convention of "allTime" or starting with "season"
-          console.log(
-            "Check the sheet names. This sheet either needs to be renamed or added into SeasonResults.js logic"
-          )
         }
-      })
+        setIsDataLoaded(true)
+      } catch (err) {
+        console.error("Error fetching data:", err)
+        setError(err.message)
+      }
     }
 
-    fetchRowData()
-  }, [sheetsAll, statsBySeason, statsByTeamSeason, dataFetched]) // updates when sheetsAll state updates
+    fetchData()
+  }, [])
 
   useEffect(() => {
-    // Return an array of all the racers and their overall stats from the allTime sheet
-    let allTimeList = []
+    const allTimeList = rowsAllTime.map((row) => ({
+      id: row.id,
+      avatar: row.avatar,
+      name: row.name,
+      allTime: {
+        participated: row.participated,
+        championships: row.championships,
+        gold: row.gold,
+        silver: row.silver,
+        bronze: row.bronze,
+        timeTrial: row.timeTrial,
+      },
+    }))
 
-    allTimeList = rowsAllTime.map((row) => {
-      let newRacer = {
-        id: row.id,
-        avatar: row.avatar,
-        name: row.name,
-        allTime: {
-          participated: row.participated,
-          championships: row.championships,
-          gold: row.gold,
-          silver: row.silver,
-          bronze: row.bronze,
-          timeTrial: row.timeTrial,
-        },
-      }
-
-      return [...allTimeList, newRacer]
-    })
-
-    const flattened = [].concat(...allTimeList)
-
-    setStatsAllTime(flattened)
-  }, [rowsAllTime, dataFetched]) // Trigger useEffect when the rowsAllTime state updates
+    setStatsAllTime([].concat(...allTimeList))
+  }, [rowsAllTime])
 
   return {
     sheetsAll,
